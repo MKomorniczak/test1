cfc0c8490c1d5aff23dcd4e7db0a34ce8a832996
retaz <retazz1991@gmail.com>
2020-12-07 15:44:44 +0100

    Hello Marek and math

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,11 +4,32 @@ package org.example;
  * Hello, Marek
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello, Marek" );
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Marek");
+
+
+        int num1 = 45;
+        int num2 = 11;
+        int additionResult = num1 + num2;
+        System.out.println("45+11=" + additionResult);
+
+        int num3=12;
+        int num4=4;
+        int multiplicationResult = num3 * num4;
+        System.out.println("12*4=" + multiplicationResult);
+
+        int num5=24;
+        int num6=6;
+        int divisionResult = num5 / num6;
+        System.out.println("24 / 6 =" + divisionResult);
+
+        int num7=55;
+        int num8=12;
+        int subtractionResult = num7 - num8;
+        System.out.println("55-12="+subtractionResult);
+
+
     }
 }
-
