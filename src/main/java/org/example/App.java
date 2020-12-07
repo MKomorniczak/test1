6db3dcf87677b2628c92441bf756def7f8280df0
retaz <retazz1991@gmail.com>
2020-12-07 14:48:16 +0100

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello, Marek
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, Marek" );
+    }
+}
+
